270595c50f612b4145fbdb2999583e850c895612
matheusmelgaco <matheusmelgacob@gmail.com>
2024-04-18 18:40:06 -0300

    classe cliente e mesa...

---
+++ codigo/Restaurante POG/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Cliente
+{
+    public string Nome { get; private set; }
+
+    public Cliente(string nome)
+    {
+        if (string.IsNullOrEmpty(nome))
+        {
+            throw new ArgumentException("Nome do cliente não pode ser nulo ou vazio.", nameof(nome));
+        }
+        Nome = nome;
+    }
+}
---
+++ codigo/Restaurante POG/Mesa.cs
@@ -0,0 +1,40 @@
+﻿public class Mesa
+{
+    public int Id { get; private set; }
+    public int Capacidade { get; private set; }
+    public bool Ocupada { get; private set; }
+
+    public Mesa(int id, int capacidade)
+    {
+        Id = id;
+        Capacidade = capacidade;
+        Ocupada = false;
+    }
+
+    public bool VerificarDisponibilidade(int qtdePessoas)
+    {
+        return !Ocupada && qtdePessoas <= Capacidade;
+    }
+
+    public void Alocar()
+    {
+        if (!Ocupada)
+        {
+            Ocupada = true;
+        }
+        else
+        {
+            throw new InvalidOperationException("Mesa já está ocupada.");
+        }
+    }
+
+    public void Desalocar()
+    {
+        Ocupada = false;
+    }
+
+    public int GetCapacidade()
+    {
+        return Capacidade;
+    }
+}
