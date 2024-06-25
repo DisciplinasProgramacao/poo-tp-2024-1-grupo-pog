8f2fde7116f69a8f4f4420e44113ac75d246c9cd
Ricardo-alencar65 <ricardocrf65@gmail.com>
2024-06-24 22:23:24 -0300

    aterações sprint 3

--- codigo/Restaurente POG V2/ItemCardapioCafe.cs
+++ codigo/Restaurente POG V2/ItemCardapioCafe.cs
@@ -1,10 +1,35 @@
 using Restaurente_POG_V2;
 using System;
 
-public abstract class ItemCardapioCafe : ItemCardapio
+namespace Restaurante_POG_V2
 {
-    protected ItemCardapioCafe(string nome, double preco)
-        : base(nome, preco)
+    public abstract class ItemCardapioCafe : ItemCardapio
     {
+        protected ItemCardapioCafe(string nome, double preco)
+            : base(nome, preco)
+        {
+        }
+    }
+
+    public class ComidaCafe : ItemCardapioCafe
+    {
+        public string Descricao { get; private set; }
+
+        public ComidaCafe(string nome, double preco, string descricao)
+            : base(nome, preco)
+        {
+            Descricao = descricao;
+        }
+    }
+
+    public class BebidaCafe : ItemCardapioCafe
+    {
+        public string Tipo { get; private set; }
+
+        public BebidaCafe(string nome, double preco, string tipo)
+            : base(nome, preco)
+        {
+            Tipo = tipo;
+        }
     }
 }
